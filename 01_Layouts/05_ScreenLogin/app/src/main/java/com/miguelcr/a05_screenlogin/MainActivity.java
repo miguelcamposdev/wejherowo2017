b5bb96546c5c87161eedba12281f8d287af3b7bf
Miguel CR <camposmiguel@gmail.com>
2017-11-01 09:53:27 +0100

    Intent
    Intent

---
+++ 01_Layouts/05_ScreenLogin/app/src/main/java/com/miguelcr/a05_screenlogin/MainActivity.java
@@ -0,0 +1,41 @@
+package com.miguelcr.a05_screenlogin;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText email, password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.editTextEmail);
+        password = findViewById(R.id.editTextPassword);
+    }
+
+    public void doLogin(View view) {
+        String e = email.getText().toString();
+        String p = password.getText().toString();
+
+        if(e.equals("admin@admin.com") && p.equals("1234")) {
+            Intent i = new Intent(
+                    this,PrivateActivity.class);
+
+            i.putExtra("userEmail",e);
+            i.putExtra("userAge",20);
+
+            startActivity(i);
+        } else {
+            Toast.makeText(
+                    this,
+                    "Try to hack another app!",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+}
---
+++ 01_Layouts/05_ScreenLogin/app/src/main/java/com/miguelcr/a05_screenlogin/PrivateActivity.java
@@ -0,0 +1,13 @@
+package com.miguelcr.a05_screenlogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrivateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_private);
+    }
+}
