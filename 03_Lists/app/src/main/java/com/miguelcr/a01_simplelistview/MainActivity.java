9181ad944664786f2ecebcc1056070f0801f500e
Miguel CR <camposmiguel@gmail.com>
2017-11-02 19:17:45 +0100

    SimpleList

--- 03_Lists/app/src/main/java/com/miguelcr/a01_simplelistview/MainActivity.java
+++ 03_Lists/app/src/main/java/com/miguelcr/a01_simplelistview/MainActivity.java
@@ -2,16 +2,20 @@ package com.miguelcr.a01_simplelistview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     ListView lista;
-    List<String> studendtList;
+    List<String> studentList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,32 +26,47 @@ public class MainActivity extends AppCompatActivity {
         lista = findViewById(R.id.listViewStudents);
 
         // 2. Create the list of elements to show
-        studendtList = new ArrayList<>();
-        studendtList.add("Heniu");
-        studendtList.add("Konrad");
-        studendtList.add("Michal");
-        studendtList.add("Kasper");
-        studendtList.add("Rafal");
-        studendtList.add("Pawel");
-        studendtList.add("Patryk");
-        studendtList.add("Grzegorz I");
-        studendtList.add("Grzegorz II");
-        studendtList.add("Kuba");
-        studendtList.add("Oskar");
-        studendtList.add("Adam");
-        studendtList.add("Alan");
-        studendtList.add("Kamil");
-        studendtList.add("Krzys");
-        studendtList.add("Dawid");
+        studentList = new ArrayList<>();
+        studentList.add("Heniu");
+        studentList.add("Konrad");
+        studentList.add("Michal");
+        studentList.add("Kasper");
+        studentList.add("Rafal");
+        studentList.add("Pawel");
+        studentList.add("Patryk");
+        studentList.add("Grzegorz I");
+        studentList.add("Grzegorz II");
+        studentList.add("Kuba");
+        studentList.add("Oskar");
+        studentList.add("Adam");
+        studentList.add("Alan");
+        studentList.add("Kamil");
+        studentList.add("Krzys");
+        studentList.add("Dawid");
 
         // 3. Adapter to connect the "ListView" with the "studentList"
         ArrayAdapter adapter = new ArrayAdapter(
                 this,
                 android.R.layout.simple_list_item_1,
-                studendtList
+                studentList
         );
 
         lista.setAdapter(adapter);
+        lista.setOnItemClickListener(this);
+    }
 
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+        String name = studentList.get(position);
+        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+
+        view.animate().rotationX(360).setDuration(2000).start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
     }
 }
