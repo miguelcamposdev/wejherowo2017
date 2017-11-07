6873ac0b13ef2e2a56d942e61961347ac0d9ba35
Miguel CR <camposmiguel@gmail.com>
2017-11-07 23:50:00 +0100

    duckhunt

--- 02_Games/02_DuckHunt/app/src/main/java/com/miguelcr/a02_duckhunt/GameActivity.java
+++ 02_Games/02_DuckHunt/app/src/main/java/com/miguelcr/a02_duckhunt/GameActivity.java
@@ -1,5 +1,6 @@
 package com.miguelcr.a02_duckhunt;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -10,7 +11,7 @@ import android.widget.TextView;
 import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
-    TextView textViewName, textViewCounter;
+    TextView textViewName, textViewCounter, textViewTimer;
     ImageView duck;
     int counter = 0;
 
@@ -24,12 +25,25 @@ public class GameActivity extends AppCompatActivity {
 
         textViewName = findViewById(R.id.textViewNick);
         textViewCounter = findViewById(R.id.textViewCounter);
+        textViewTimer = findViewById(R.id.textViewTimer);
+
         duck = findViewById(R.id.imageViewDuck);
 
         textViewName.setText(nickName);
 
         // Hide the action bar
         getSupportActionBar().hide();
+
+        new CountDownTimer(60000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                textViewTimer.setText(millisUntilFinished / 1000+"s");
+            }
+
+            public void onFinish() {
+                textViewTimer.setText("Game over!");
+            }
+        }.start();
     }
 
     public void duckClick(View view) {
@@ -49,8 +63,11 @@ public class GameActivity extends AppCompatActivity {
         int heightPixels = metrics.heightPixels;
         int widthPixels = metrics.widthPixels;
 
-        int maxX = widthPixels;
-        int maxY = heightPixels;
+        int duckWidth = duck.getWidth();
+        int duckHeight = duck.getHeight();
+
+        int maxX = widthPixels-duckWidth;
+        int maxY = heightPixels-duckHeight;
         int min = 0;
         int randomX = rand.nextInt((maxX - min) + 1) + min;
         int randomY = rand.nextInt((maxY - min) + 1) + min;
